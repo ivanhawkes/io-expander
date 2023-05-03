77f0cbe40b33d6587cd9c226073694ece9d33381
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-05-04 03:17:30 +1000

    It's reading from PORT B and writing to PORTA now. Test working at 400khz. Failed at 1.7mhz. No surprise - it's on a breadboard.

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,34 +7,15 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
-#include "hardware/i2c.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
+#include "MCP23017.h"
 
-#include <MCP23017.h>
-
-const uint kClockPin = 3;
-const uint kData01Pin = 4;
-const uint kData02Pin = 5;
-const uint kData03Pin = 6;
-const uint kData04Pin = 7;
-
-// I2C address
-static const uint8_t BASE_ADDRESS = 0x20;
-static const uint8_t ADDRESS_FIRST = BASE_ADDRESS;
-static const uint8_t ADDRESS_SECOND = BASE_ADDRESS + 1;
-static const uint8_t ADDRESS_THIRD = BASE_ADDRESS + 2;
-static const uint8_t ADDRESS_FOURTH = BASE_ADDRESS + 3;
-
-// Registers
-static const uint8_t REG_DEVID = 0x00;
-static const uint8_t REG_POWER_CTL = 0x2D;
-static const uint8_t REG_DATAX0 = 0x32;
-
-// Other constants
-static const uint8_t DEVID = 0xE5;
-static const float SENSITIVITY_2G = 1.0 / 256; // (g/LSB)
-static const float EARTH_GRAVITY = 9.80665;    // Earth's gravity in [m/s^2]
+// I2C addresses. Assumption: we have four units on a board. The limit is eight,
+// but we don't have a board for that yet.
+static const uint8_t kBaseAddress = 0x20;
+static const uint8_t kAddressFirst = kBaseAddress;
+static const uint8_t kAddressSecond = kBaseAddress + 1;
+static const uint8_t kAddressThird = kBaseAddress + 2;
+static const uint8_t kAddressFourth = kBaseAddress + 3;
 
 // Ports
 i2c_inst_t *i2c = i2c0;
@@ -46,9 +27,9 @@ void Blink() {
   // Simple method to blink the default LED.
   // FIXME: This is completely wasteful.
   gpio_put(PICO_DEFAULT_LED_PIN, 1);
-  sleep_ms(50);
+  sleep_ms(100);
   gpio_put(PICO_DEFAULT_LED_PIN, 0);
-  sleep_ms(50);
+  sleep_ms(100);
 }
 
 // Write 1 byte to the specified register
@@ -94,39 +75,21 @@ int reg_read(i2c_inst_t *i2c, const uint addr, const uint8_t reg, uint8_t *buf,
   return num_bytes_read;
 }
 
-void Loop2() {
-  int16_t acc_x;
-  int16_t acc_y;
-  int16_t acc_z;
-  float acc_x_f;
-  float acc_y_f;
-  float acc_z_f;
+void Loop() {
+  // Read from PORTB.
+  data[0] = 0x13;
+  data[1] = 0xAA; // TEST: random garbage
+  i2c_write_blocking(i2c, kAddressFirst, data, 1, true);
+  i2c_read_blocking(i2c, kAddressFirst, data, 1, false);
+  printf("value = 0x%0X\r\n", data[0]);
+  data[1] = data[0]; // Copy the returned result for output.
 
-  // Read X, Y, and Z values from registers (16 bits each)
-  reg_read(i2c, BASE_ADDRESS, REG_DATAX0, data, 6);
-
-  // Convert 2 bytes (little-endian) into 16-bit integer (signed)
-  acc_x = (int16_t)((data[1] << 8) | data[0]);
-  acc_y = (int16_t)((data[3] << 8) | data[2]);
-  acc_z = (int16_t)((data[5] << 8) | data[4]);
-
-  // Convert measurements to [m/s^2]
-  acc_x_f = acc_x * SENSITIVITY_2G * EARTH_GRAVITY;
-  acc_y_f = acc_y * SENSITIVITY_2G * EARTH_GRAVITY;
-  acc_z_f = acc_z * SENSITIVITY_2G * EARTH_GRAVITY;
-
-  // Print results
-  printf("X: %.2f | Y: %.2f | Z: %.2f\r\n", acc_x_f, acc_y_f, acc_z_f);
-
-  sleep_ms(100);
+  // Write the previously read value to PORTA.
+  data[0] = 0x12;
+  i2c_write_blocking(i2c, kAddressFirst, data, 2, false);
 }
 
-// ***
-// *** main
-// ***
-
-int main() {
-
+void Initialise() {
   // Init the USB / UART IO.
   stdio_init_all();
 
@@ -134,7 +97,7 @@ int main() {
   stdio_uart_init_full(uart0, 115200, 16, 17);
 
   // Initialize I2C port at 400 kHz.
-  uint actualRate = i2c_init(i2c, 100 * 1000);
+  uint actualRate = i2c_init(i2c, 400 * 1000);
 
   printf("\n\nIO Expander: Rate = %u\n\n", actualRate);
 
@@ -150,44 +113,36 @@ int main() {
 
   printf("\n\nPre-write\n\n");
 
-  // Set PORTA to input mode.
+  // Set PORTA to output mode.
   data[0] = 0x00; // MCP23017::ControlRegister::IODirectionB;
   data[1] = 0x00;
-  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
+  i2c_write_blocking(i2c, kAddressFirst, data, 2, false);
 
-  // Set PORTA pullups to ON.
+  // Set PORTA pullups to OFF.
   data[0] = 0x0C;
-  data[1] = 0xFF;
-  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
+  data[1] = 0x00;
+  i2c_write_blocking(i2c, kAddressFirst, data, 2, false);
 
-  // Set PORTB to output mode.
+  // Set PORTB to input mode.
   data[0] = 0x01;
   data[1] = 0xFF;
-  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
+  i2c_write_blocking(i2c, kAddressFirst, data, 2, false);
 
-  // Read from PORTA.
-  data[0] = 0x12;
-  data[1] = 0xAA;
-  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, true);
-  int num_bytes_read = i2c_read_blocking(i2c, ADDRESS_FIRST, data, 1, false);
-  printf("Return = %d bytes, value = %u\r\n", num_bytes_read, data[0]);
+  // Set PORTB pullups to ON.
+  data[0] = 0x0D;
+  data[1] = 0xFF;
+  i2c_write_blocking(i2c, kAddressFirst, data, 2, false);
 
-  // Write to PORTB.
-  data[0] = 0x13;
-  data[1] = 0x0F;
-  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
+  printf("\n\nInit complete!\n\n");
+}
 
-  printf("\n\nMade it through!\n\n");
+int main() {
 
-  // Wait before taking measurements
-  sleep_ms(2000);
+  Initialise();
 
   // Loop forever
   while (true) {
-
-    // Loop2();
-    //    printf("...\n");
-
+    Loop();
     Blink();
   }
 }
