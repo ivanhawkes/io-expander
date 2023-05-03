b671a2ad74eca5b5f9ccabcfdf6c0dee4d2d72cb
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-05-04 02:32:38 +1000

    Finally have some code that runs while talking to the chip. It's doing the opposite of what I expected, but that's still a step forward.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -18,12 +19,12 @@ const uint kData02Pin = 5;
 const uint kData03Pin = 6;
 const uint kData04Pin = 7;
 
-// Pins
-const uint kSDAPin = 4;
-const uint kSCLPin = 5;
-
 // I2C address
 static const uint8_t BASE_ADDRESS = 0x20;
+static const uint8_t ADDRESS_FIRST = BASE_ADDRESS;
+static const uint8_t ADDRESS_SECOND = BASE_ADDRESS + 1;
+static const uint8_t ADDRESS_THIRD = BASE_ADDRESS + 2;
+static const uint8_t ADDRESS_FOURTH = BASE_ADDRESS + 3;
 
 // Registers
 static const uint8_t REG_DEVID = 0x00;
@@ -133,40 +134,50 @@ int main() {
   stdio_uart_init_full(uart0, 115200, 16, 17);
 
   // Initialize I2C port at 400 kHz.
-  i2c_init(i2c, 400 * 1000);
+  uint actualRate = i2c_init(i2c, 100 * 1000);
 
-  printf("\n\nIO Expander Test newt\n\n");
+  printf("\n\nIO Expander: Rate = %u\n\n", actualRate);
 
   // Control the on-board LED so we have a heartbeat to confirm operation.
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
   // Initialize I2C pins
-  gpio_set_function(kSDAPin, GPIO_FUNC_I2C);
-  gpio_set_function(kSCLPin, GPIO_FUNC_I2C);
+  gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+  gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+  gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
-  printf("\n\nWTF\n\n");
-  // Read device ID to make sure that we can communicate with the chip.
-  reg_read(i2c, BASE_ADDRESS, REG_DEVID, data, 1);
-  if (data[0] != DEVID) {
-    printf("ERROR: Could not communicate with IO Expander\r\n");
-    while (true)
-      ;
-  }
+  printf("\n\nPre-write\n\n");
 
-  printf("DEVID: %i\r\n", data[0]);
+  // Set PORTA to input mode.
+  data[0] = 0x00; // MCP23017::ControlRegister::IODirectionB;
+  data[1] = 0x00;
+  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
 
-  // // Read Power Control register
-  // reg_read(i2c, BASE_ADDRESS, REG_POWER_CTL, data, 1);
-  // printf("0x%X\r\n", data[0]);
+  // Set PORTA pullups to ON.
+  data[0] = 0x0C;
+  data[1] = 0xFF;
+  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
 
-  // // Tell ADXL343 to start taking measurements by setting Measure bit to high
-  // data[0] |= (1 << 3);
-  // reg_write(i2c, BASE_ADDRESS, REG_POWER_CTL, &data[0], 1);
+  // Set PORTB to output mode.
+  data[0] = 0x01;
+  data[1] = 0xFF;
+  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
 
-  // // Test: read Power Control register back to make sure Measure bit was set
-  // reg_read(i2c, BASE_ADDRESS, REG_POWER_CTL, data, 1);
-  // printf("0x%X\r\n", data[0]);
+  // Read from PORTA.
+  data[0] = 0x12;
+  data[1] = 0xAA;
+  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, true);
+  int num_bytes_read = i2c_read_blocking(i2c, ADDRESS_FIRST, data, 1, false);
+  printf("Return = %d bytes, value = %u\r\n", num_bytes_read, data[0]);
+
+  // Write to PORTB.
+  data[0] = 0x13;
+  data[1] = 0x0F;
+  i2c_write_blocking(i2c, ADDRESS_FIRST, data, 2, false);
+
+  printf("\n\nMade it through!\n\n");
 
   // Wait before taking measurements
   sleep_ms(2000);
@@ -175,111 +186,8 @@ int main() {
   while (true) {
 
     // Loop2();
+    //    printf("...\n");
 
     Blink();
   }
 }
-
-// void Loop() {
-//   uint8_t data01{0};
-//   uint8_t data02{0};
-//   uint8_t data03{0};
-//   uint8_t data04{0};
-
-//   // Sample the pins.
-//   //???
-//   sleep_us(1);
-//   //???
-//   sleep_us(1);
-
-//   for (int i = 0; i < 8; i++) {
-//     // Read data.
-//     bool q1 = gpio_get(kData01Pin);
-//     data01 = data01 << 1;
-//     data01 += q1 ? 1 : 0;
-
-//     // Read data.
-//     bool q2 = gpio_get(kData02Pin);
-//     data02 = data02 << 1;
-//     data02 += q2 ? 1 : 0;
-
-//     // Read data.
-//     bool q3 = gpio_get(kData03Pin);
-//     data03 = data03 << 1;
-//     data03 += q3 ? 1 : 0;
-
-//     // Read data.
-//     bool q4 = gpio_get(kData04Pin);
-//     data04 = data04 << 1;
-//     data04 += q4 ? 1 : 0;
-
-//     sleep_us(1);
-
-//     // Tick tock.
-//     gpio_put(kClockPin, 1);
-//     sleep_us(1);
-//     gpio_put(kClockPin, 0);
-//     sleep_us(1);
-//   }
-
-//   // Get the results in binary.
-//   char buffer01[10];
-//   char buffer02[10];
-//   char buffer03[10];
-//   char buffer04[10];
-//   itoa(data01, buffer01, 2);
-//   itoa(data02, buffer02, 2);
-//   itoa(data03, buffer03, 2);
-//   itoa(data04, buffer04, 2);
-
-//   printf("01: 0x%0X %s    02: 0x%0X %s    03: 0x%0X %s    04: 0x%0X %s\n",
-//          data01, buffer01, data02, buffer02, data03, buffer03, data04,
-//          buffer04);
-// }
-
-// ***
-// *** NOTE: Just hacking the shift register code to get the form laid out. Will
-// *** need major changes before this executes.
-// ***
-
-// int mainOld(void) {
-//   // Init the USB / UART IO.
-//   stdio_init_all();
-
-//   // Init the uart using our preferred settings.
-//   stdio_uart_init_full(uart0, 115200, 16, 17);
-
-//   printf("\n\nIO Expander Test old\n\n");
-
-//   // Control the on-board LED so we have a heartbeat to confirm operation.
-//   gpio_init(PICO_DEFAULT_LED_PIN);
-//   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
-//   // Set the pins.
-//   gpio_init(kClockPin);
-//   gpio_set_dir(kClockPin, GPIO_OUT);
-
-//   gpio_init(kData01Pin);
-//   gpio_set_dir(kData01Pin, GPIO_IN);
-
-//   gpio_init(kData02Pin);
-//   gpio_set_dir(kData02Pin, GPIO_IN);
-
-//   gpio_init(kData03Pin);
-//   gpio_set_dir(kData03Pin, GPIO_IN);
-
-//   gpio_init(kData04Pin);
-//   gpio_set_dir(kData04Pin, GPIO_IN);
-
-//   // Set the initial state.
-//   gpio_put(kClockPin, 0);
-//   sleep_us(1);
-
-//   while (true) {
-//     Loop();
-
-//     Blink();
-//   }
-
-//   return 0;
-// }
