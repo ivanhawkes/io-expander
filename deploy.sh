b798476c4fd73f18eeb77c4e4e3c1dd8e2a831a9
Ivan Hawkes <ivan.hawkes@gmail.com>
2023-10-20 01:15:38 +1000

    Changed the speed of the connection.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo Deploying IO Expander test to the Pico.
 script="$0"
 basename="${script%/*}"
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 4000" -c "program ${basename}/build/src/io-expander.elf verify reset exit"
+openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program ${basename}/build/src/io-expander.elf verify reset exit"
 
